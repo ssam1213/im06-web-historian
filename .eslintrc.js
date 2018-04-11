2963486ad2ae0a6c59fe9c732fc3767217f12633
deanheo <hideanheo@gmail.com>
2018-04-12 00:29:17 +0900

    init

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+/**
+ * These rules enforce Code States's style guide.
+ * Visit this repo for more information:
+ *   https://github.com/codestates/eslint-config-codestates
+ */
+
+module.exports = {
+  extends: './node_modules/eslint-config-codestates/index.js'
+};
